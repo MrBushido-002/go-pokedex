e4469aa24a3ba25059ab81a71692f665918338f2
Connor Watson <connorwatson@Tammis-MacBook-Pro.local>
2026-02-02 15:50:37 -0600

    small update

--- commandPokedex.go
+++ commandPokedex.go
@@ -5,8 +5,9 @@ import (
 )
 
 func commandPokedex(cfg *config, args []string) error {
+	fmt.Println("Your Pokedex:\n")
 	for _, val := range cfg.caughtPokemon {
-		fmt.Printf("%v\n", val.Name)
+		fmt.Printf(" - %v\n", val.Name)
 	}
 	return nil
 }
